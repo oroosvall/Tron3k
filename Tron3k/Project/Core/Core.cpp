6a5b6ad8f3421e063d0413447e39c82d2e77a3a9
ResQQ <resq@live.se>
2015-11-09 23:48:48 +0100

    gamestate draft

--- Tron3k/Project/Core/Core.cpp
+++ Tron3k/Project/Core/Core.cpp
@@ -7,7 +7,82 @@ void Core::init()
 
 void Core::update(float dt)
 {
-	//gamstates och saker
+	//update I/O
 
+	switch (current)
+	{
+	case START:		upStart(dt);	break;
+	case MENU:		upMenu(dt);		break;
+	case ROAM:		upRoam(dt);		break;
+	case CLIENT:	upClient(dt);	break;
+	case SERVER:	upServer(dt);	break;
+	default:						break;
+	}
 
+	if (current != START && current != SERVER)
+	{
+		//update ui & sound
+		//update engine
+	}
+}
+
+void Core::upStart(float dt)
+{
+	//START
+	//server -> SERVER
+	//client -> MENU
+
+	//return exit
+}
+
+void Core::upMenu(float dt)
+{
+	//MENU
+
+	//menu opitons
+
+	//roam, choose map -> ROAM
+	//multiplayer, enter server IP -> CLIENT
+	//settings
+	//exit
+	
+	//return exit
+}
+
+void Core::upRoam(float dt)
+{
+	//ROAM
+	//load
+	//run
+
+	//return MENU
+}
+
+void Core::upClient(float dt)
+{
+	//CLIENT
+	//connect
+	//map valid ?
+	//load
+	//get full server info
+	//run
+	//network in / out
+	//spectator
+	//pick team
+	//wait for server start event
+
+	//return MENU
+}
+
+void Core::upServer(float dt)
+{
+	//SERVER
+	//bind port ?
+	//pick map
+	//load map
+	//run
+	//look for clinets
+	//network in / out
+
+	//return START
 }
--- Tron3k/Project/Core/Core.h
+++ Tron3k/Project/Core/Core.h
@@ -11,6 +11,16 @@
 #include "../../Engine/IEngine.h"
 #include "../../Engine/Game/IGame.h"
 
+enum Gamestate
+{
+	START,
+	MENU,
+	ROAM,
+	CLIENT,
+	SERVER
+};
+
+
 class Core
 {
 private:
@@ -18,6 +28,18 @@ private:
 	IGame* game;
 	IEngine* engine;
 
+	//gamestates
+	Gamestate current;
+	Gamestate last;
+	int subState;
+	int lastSubState;
+
+	//gamestate update functions
+	void upStart(float dt);
+	void upMenu(float dt);
+	void upRoam(float dt);
+	void upClient(float dt);
+	void upServer(float dt);
 public:
 	void init();
 	void update(float dt);
