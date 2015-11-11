7361eee91a763bc9a2b3e4451a2ced894c6c3dd3
joeljoeljoeljoeljoel <munken_92@yahoo.se>
2015-11-11 16:08:12 +0100

    Sound shit

--- Tron3k/Project/Core/sound/SoundPlayer.cpp
+++ Tron3k/Project/Core/sound/SoundPlayer.cpp
@@ -1,8 +1,9 @@
 #include "SoundPlayer.h"
 
+
 SoundPlayer::SoundPlayer()
 {
-
+	soundPlayer.setAttenuation(10.0f); //NEW! Sets the global attenuation of all the sounds.
 }
 
 SoundPlayer::~SoundPlayer()
@@ -10,13 +11,32 @@ SoundPlayer::~SoundPlayer()
 
 }
 
-int SoundPlayer::playSound(int sound)
+int SoundPlayer::playUserGeneratedSound(int sound)
 {
+	
 	if (!soundBuffer.loadFromFile("tester.ogg"))
 	{
 		return -1;
 	}
+	
+	soundPlayer.setBuffer(soundBuffer);
+	soundPlayer.play();
 
+	return 0;
+}
+
+int SoundPlayer::playExternalSound(int sound, glm::vec3 soundOrigin)
+{
+	//sf::Listener::setPosition(x, y, z);			Set the position of the player
+	//sf::Listener::setDirection(x, y, z);			Set the direction of the player
+
+	if (!soundBuffer.loadFromFile("tester.ogg"))
+	{
+		return -1;
+	}
+	
+	//soundPlayer.setMinDistance(parameter);		Set the sound's distance it travels before it starts to attenuate. Could be passed in through a parameter.
+	//soundPlayer.setPosition(soundOrigin):			Set the sound's position in the world. Could be passed in through a parameter.
 	soundPlayer.setBuffer(soundBuffer);
 	soundPlayer.play();
 
@@ -25,11 +45,11 @@ int SoundPlayer::playSound(int sound)
 
 int SoundPlayer::playMusic(int music)
 {
-	if (!musicPlayer.openFromFile("musicTester.ogg"))
+	if (!musicPlayer.openFromFile("musicTester1.ogg"))
 	{
 		return -1;
 	}
-
+	
 	musicPlayer.play();
 
 	return 0;
