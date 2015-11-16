4930b1cf672882af3196c1233361d7b05a7ea586
Datlof <carcity.h@gmail.com>
2015-11-16 14:24:32 +0100

    Collision math added

--- Tron3k/Physics/Collision/Geometry.h
+++ Tron3k/Physics/Collision/Geometry.h
@@ -1,14 +1,25 @@
 #ifndef GEOMETRY_H
 #define GEOMETRY_H
 
+#include"../../Dependencies/glm/glm/glm.hpp"
+
 class Geometry
 {
 private:
 	//Geometry boxes go here
 	//TODO: Load a bounding box, probably through the way we load files
+
+	glm::vec3 pos = glm::vec3(0, 0, 0);
+	glm::vec3 size = glm::vec3(0, 0, 0);
 public:
-	Geometry();
+	Geometry(glm::vec3 pos, glm::vec3 size);
 	~Geometry();
+
+	const glm::vec3 getPos() { return pos; }
+	const glm::vec3 getSize() { return size; }
+
+	void setPos(glm::vec3 pos) { this->pos = pos; }
+	void setSize(glm::vec3 size) { this->size = size; }
 
 };
 #endif
