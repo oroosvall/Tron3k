4a52f3aa46712ee9de0d205ccc26f01b5ad7d341
Pristin <martin_nygren9405@hotmail.com>
2015-11-20 15:12:11 +0100

    Lite TXT :dancers:

--- Tron3k/Project/Core/Game/Role/Role.h
+++ Tron3k/Project/Core/Game/Role/Role.h
@@ -2,17 +2,18 @@
 #define ROLE_H
 
 #include "Weapon\weapon.h"
+#include "../GameDataIndex.h"
 #include<string>
 
 using namespace std;
 
-enum PLAYERMSG { TRAPPER, DESTROYER, MOBILITY, BRUTE, MANIPULATOR };
+enum PLAYERMSG { TRAPPER, DESTROYER, MOBILITY, BRUTE, MANIPULATOR, NROFROLES };
 
 class Role
 {
 	private:
 		int currentWpn = 0;
-		string roles[5];
+		string roles[NROFROLES];
 
 		Weapon weapons[2];
 		Weapon pickup;
