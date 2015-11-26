73a341bd12ac50703d4af91857c0a7fd3e28c9f0
ResQQ <resq@live.se>
2015-11-26 09:54:04 +0100

    new glow

--- Tron3k/RenderPipeline/ContentManager.cpp
+++ Tron3k/RenderPipeline/ContentManager.cpp
@@ -115,11 +115,15 @@ ContentManager::~ContentManager()
 
 void ContentManager::renderChunks(GLuint shader, GLuint shaderLocation, GLuint textureLocation, GLuint normalLocation, GLuint glowSpecLocation)
 {
-	//glActiveTexture(GL_TEXTURE0);
-	//glBindTexture(GL_TEXTURE_2D, textures[1].textureID);
-	//glActiveTexture(GL_TEXTURE0 +1 );
-	//glBindTexture(GL_TEXTURE_2D, textures[0].textureID);
-	//testMap.render(shader, shaderLocation);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, textures[1].textureID);
+	glActiveTexture(GL_TEXTURE0 +1 );
+	glBindTexture(GL_TEXTURE_2D, textures[0].textureID);
+	//static glow
+	glActiveTexture(GL_TEXTURE0 + 2);
+	glBindTexture(GL_TEXTURE_2D, textures[5].textureID);
+	testMap.render(shader, shaderLocation);
+
 	for (size_t i = 0; i < meshes.size(); i++)
 	{
 		glProgramUniformMatrix4fv(shader, shaderLocation, 1, GL_FALSE, (GLfloat*)meshes[i].getWorld());
