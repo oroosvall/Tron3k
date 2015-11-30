e90cab9ea4e0d548cab5edf60562a0ca1512f587
Pristin <martin_nygren9405@hotmail.com>
2015-11-30 12:47:41 +0100

    test genomfört ... reverting

--- Tron3k/SoundPlayer/sound/SoundPlayer.cpp
+++ Tron3k/SoundPlayer/sound/SoundPlayer.cpp
@@ -98,7 +98,7 @@ int SoundPlayer::playExternalSound(int sound, float x, float y, float z)
 	sf::Listener::setDirection(playerDir.x, playerDir.y, playerDir.z);			//Set the direction of the player
 
 	sounds[nrOfSoundsPlaying].setMinDistance(10.0f);		//Set the sound's distance it travels before it starts to attenuate. Could be passed in through a parameter.
-	sounds[nrOfSoundsPlaying].setPosition(0, 0, 0);			//Set the sound's position in the world. Could be passed in through a parameter.
+	sounds[nrOfSoundsPlaying].setPosition(x, y, z);			//Set the sound's position in the world. Could be passed in through a parameter.
 	sounds[nrOfSoundsPlaying].setBuffer(soundList[sound]);
 	sounds[nrOfSoundsPlaying].setVolume(soundVolume);
 	sounds[nrOfSoundsPlaying].play();
