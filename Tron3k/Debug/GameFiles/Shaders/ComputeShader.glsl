90edac62cd74b1a7fd45c900eaafcfa0ee7d1c95
deol13 <dennan_lol@hotmail.com>
2015-12-01 11:16:46 +0100

    Skeleton Shader Done

---
+++ Tron3k/Debug/GameFiles/Shaders/ComputeShader.glsl
@@ -0,0 +1,39 @@
+#version 430
+
+struct bones
+{
+	vec3 pos;
+	vec2 uv;
+	vec3 normal;
+	vec3 tangent;
+	vec4 boneIndices;
+	vec4 skinWeights;
+};
+
+layout (std430, binding = 2) buffer boneArray
+{
+	bones data[];
+};	
+
+layout (local_size_x = 20, local_size_y = 1, local_size_z = 1)in;  //This row isn't done
+
+void main()
+{
+	uint boneId =  gl_GlobalInvocationID.x;
+
+	bones[boneId].pos = mul(vec4(bones[boneId].pos, 1.0f), inverseBindpose[bones[boneId].boneIndices.x]) * bones[boneId].skinWeights.x;
+	bones[boneId].normal = mul(vec4(bones[boneId].normal, 1.0f), inverseBindpose[bones[boneId].boneIndices.x]) * bones[boneId].skinWeights.x;
+	bones[boneId].tangent = mul(vec4(bones[boneId].tangent, 1.0f), inverseBindpose[bones[boneId].boneIndices.x]) * bones[boneId].skinWeights.x;
+	
+	bones[boneId].pos = mul(vec4(bones[boneId].pos, 1.0f), inverseBindpose[bones[boneId].boneIndices.y]) * bones[boneId].skinWeights.y;
+	bones[boneId].normal = mul(vec4(bones[boneId].normal, 1.0f), inverseBindpose[bones[boneId].boneIndices.y]) * bones[boneId].skinWeights.y;
+	bones[boneId].tangent = mul(vec4(bones[boneId].tangent, 1.0f), inverseBindpose[bones[boneId].boneIndices.y]) * bones[boneId].skinWeights.y;
+	
+	bones[boneId].pos = mul(vec4(bones[boneId].pos, 1.0f), inverseBindpose[bones[boneId].boneIndices.z]) * bones[boneId].skinWeights.z;
+	bones[boneId].normal = mul(vec4(bones[boneId].normal, 1.0f), inverseBindpose[bones[boneId].boneIndices.z]) * bones[boneId].skinWeights.z;
+	bones[boneId].tangent = mul(vec4(bones[boneId].tangent, 1.0f), inverseBindpose[bones[boneId].boneIndices.z]) * bones[boneId].skinWeights.z;
+	
+	bones[boneId].pos = mul(vec4(bones[boneId].pos, 1.0f), inverseBindpose[bones[boneId].boneIndices.w]) * bones[boneId].skinWeights.w;
+	bones[boneId].normal = mul(vec4(bones[boneId].normal, 1.0f), inverseBindpose[bones[boneId].boneIndices.w]) * bones[boneId].skinWeights.w;
+	bones[boneId].tangent = mul(vec4(bones[boneId].tangent, 1.0f), inverseBindpose[bones[boneId].boneIndices.w]) * bones[boneId].skinWeights.w;
+}
--- Tron3k/Debug/GameFiles/Shaders/SkeletonAnimation_vs.glsl
+++ Tron3k/Debug/GameFiles/Shaders/SkeletonAnimation_vs.glsl
@@ -1,16 +1,23 @@
 #version 410
-layout(location = 0);
-layout(location = 1);
-layout(location = 2);
-layout(location = 3);
-layout(location = 4);
-layout(location = 5);
+layout(location = 0) in vec3 bonePos;
+layout(location = 1) in vec2 boneUV;
+layout(location = 2) in vec3 boneNormal;
+layout(location = 3) in vec3 boneTagent;
 
+uniform mat4 WorldMatrix;
 uniform mat4 ViewProjMatrix;
 uniform mat4 BoneMatrices[100];
-uniform int BoneMatricesIndices[];
+
+layout(location = 0) out vec3 bonePos0;
+layout(location = 1) out vec2 boneUV0;
+layout(location = 2) out vec3 boneNormal0;
+layout(location = 3) out vec3 boneTagent0;
 
 void main()
 {
-	gl_Position = ViewProjMatrix * (vec4(PositionOut, 1.0) * BoneMatrices[]);
+	bonePos0 = (vec4(bonePos, 1.0f) * WorldMatrix).xyz;
+	gl_Position = ViewProjMatrix * vec4(bonePos0, 1.0);
+	boneUV0 = boneUV;
+	boneNormal0 = boneNormal;
+	boneTagent0 = boneTagent;
 }
