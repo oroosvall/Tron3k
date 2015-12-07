28e5f76d6a00901f1230562a08e993d66eb6039a
ResQQ <resq@live.se>
2015-12-07 23:43:02 +0100

    interpolate length in portal shader (looks better)

--- Tron3k/Debug/GameFiles/Shaders/portal_fs.glsl
+++ Tron3k/Debug/GameFiles/Shaders/portal_fs.glsl
@@ -1,6 +1,6 @@
 #version 410
 
-layout(location = 0) in float dist;
+layout(location = 0) in vec4 dist;
 
 layout (location = 1) out vec4 WorldPosOut;   
 layout (location = 2) out vec4 DiffuseOut;     
@@ -9,16 +9,16 @@ layout (location = 4) out vec4 GlowMap;
 
 void main () 
 {
-	float maxlen = 100;
-	float transp = dist;
+	float maxlen = 20;
+	float transp = length(dist);
 	
-	if(transp > maxlen * 0.9)
-		transp = 0.1;
+	if(transp > maxlen)
+		transp = 0.0;
 	else
 		transp =  1 - (transp / maxlen);
   
 	WorldPosOut	= vec4(0);
-	DiffuseOut = vec4((1 - transp) / 1.5 , 0 , transp / 2, 0.25 + transp / 4);	
+	DiffuseOut = vec4(1.25 - transp , 0 , transp, transp / 4);	
 	NormalOut = vec4(0);
 	GlowMap = vec4(0);
 }
