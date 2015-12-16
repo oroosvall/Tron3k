ea4f207aaf3aac00ffa9e00315bb5c3cef9b0f12
joeljoeljoeljoeljoel <munken_92@yahoo.se>
2015-12-16 13:58:42 +0100

    Shotgun cock

--- Tron3k/SoundPlayer/sound/SoundPlayer.cpp
+++ Tron3k/SoundPlayer/sound/SoundPlayer.cpp
@@ -226,6 +226,21 @@ void SoundPlayer::playFootsteps(int role, float posX, float posY, float posZ)
 			playExternalSound(SOUNDS::soundFootSteps, posX, posY, posZ);
 		}
 
+		if (role == 1)
+		{
+			playExternalSound(SOUNDS::soundFootSteps, posX, posY, posZ);
+		}
+
+		if (role == 2)
+		{
+			playExternalSound(SOUNDS::soundFootSteps, posX, posY, posZ);
+		}
+
+		if (role == 3)
+		{
+			playExternalSound(SOUNDS::soundFootSteps, posX, posY, posZ);
+		}
+
 		if (role == 4)
 		{
 			playExternalSound(SOUNDS::soundStepsManipulator, posX, posY, posZ);
