84e7b31f56288a6e295d05d087c3ed523447aa9c
Kraetyz <krae_tyz@hotmail.com>
2016-01-19 10:55:07 +0100

    BatteryFields logic

--- Tron3k/Project/Core/Game/Role/Weapon/WeaponTypes/Manipulator/BatteryFields.cpp
+++ Tron3k/Project/Core/Game/Role/Weapon/WeaponTypes/Manipulator/BatteryFields.cpp
@@ -10,9 +10,37 @@ BatteryFields::~BatteryFields()
 
 }
 
+bool BatteryFields::shoot()
+{
+	bool ableToShoot = false;
+
+	if (rldTimer < FLT_EPSILON)
+	{
+		if (firingSpeedCurrentDelay < FLT_EPSILON && currentClipAmmo > 0)
+		{
+			currentClipAmmo--;
+			firingSpeedCurrentDelay = firingSpeed;
+			ableToShoot = true;
+			if (weaponType == WEAPON_TYPE::BATTERYFIELD_SLOW)
+				weaponType = WEAPON_TYPE::BATTERYFIELD_SPEED;
+			else if (weaponType == WEAPON_TYPE::BATTERYFIELD_SPEED)
+				weaponType = WEAPON_TYPE::BATTERYFIELD_SLOW;
+		}
+	}
+
+	return ableToShoot;
+}
+
+bool BatteryFields::reload()
+{
+	rldTimer = reloadTime;
+	weaponType = WEAPON_TYPE::BATTERYFIELD_SPEED;
+	return true;
+}
+
 void BatteryFields::init()
 {
-	weaponType = WEAPON_TYPE::BATTERYFIELD_SLOW;
+	weaponType = WEAPON_TYPE::BATTERYFIELD_SPEED;
 
 	maxClipSize = 2;
 	currentClipAmmo = maxClipSize;
