1520fca1ed6be2a5e7b2aa1de260e79a8ca6a22a
Pristin <martin_nygren9405@hotmail.com>
2016-01-22 14:08:40 +0100

    Rampfactor //Adam

--- Tron3k/Project/Core/Game/Player.cpp
+++ Tron3k/Project/Core/Game/Player.cpp
@@ -141,7 +141,7 @@ void Player::movePlayerCollided(float dt, glm::vec3 oldDir, bool freecam, bool s
 
 			//ramp factor and grounded
 			
-			if (collisionNormals[k].y > 0.5f)
+			if (collisionNormals[k].y > 0.4f)
 			{
 				grounded = true;
 			}
