f3fbf9a94b8a3043a4ad78b0ebf4b954fdb379f5
Pristin <martin_nygren9405@hotmail.com>
2016-01-22 10:32:43 +0100

    Lila hackingdart

--- Tron3k/Project/Core/Core.cpp
+++ Tron3k/Project/Core/Core.cpp
@@ -1048,11 +1048,11 @@ void Core::renderWorld(float dt)
 							dgColor[0] = 0; dgColor[1] = 0; dgColor[2] = 0;
 						}
 						//hacked team colors
-						if (hackedTeam == 1) {
-							dgColor[0] = 0.2f; dgColor[1] = 0.2f; dgColor[2] = 1;
+						if (hackedTeam == 1) { //Show team 2's colour
+							dgColor[0] = 0.4f; dgColor[1] = 0.0f; dgColor[2] = 0.4f;
 						}
-						else if (hackedTeam == 2) {
-							dgColor[0] = 0; dgColor[1] = 1; dgColor[2] = 0;
+						else if (hackedTeam == 2) { //Show team 1's colour
+							dgColor[0] = 0.0f; dgColor[1] = 1.0f; dgColor[2] = 0.0f;
 						}
 					}
 					//static intense based on health
