f08770e2c4eec165613e92d5f6ffd07dd29e7950
Pristin <martin_nygren9405@hotmail.com>
2016-01-26 13:41:58 +0100

    you saw nothing

--- Tron3k/Project/Core/Game/Game.cpp
+++ Tron3k/Project/Core/Game/Game.cpp
@@ -1058,13 +1058,13 @@ Bullet* Game::getSpecificBullet(int PID, int BID, BULLET_TYPE bt, int &posInBull
 
 int Game::handleBulletHitPlayerEvent(BulletHitPlayerInfo hi)
 {
+	glm::vec3 pos = playerList[hi.playerHit]->getPos();
+	Player* p = playerList[hi.playerHit];
 	if (hi.bt != BULLET_TYPE::CLUSTERLING)	//Any bullets that should not detonate on contact
 	{
-		glm::vec3 pos = playerList[hi.playerHit]->getPos();
 		if (gameState != Gamestate::SERVER)
 			if (GetSoundActivated())
 				GetSound()->playExternalSound(SOUNDS::soundEffectBulletPlayerHit, pos.x, pos.y, pos.z);
-		Player* p = playerList[hi.playerHit];
 		int bulletPosInArray;
 		Bullet* theBullet = getSpecificBullet(hi.bulletPID, hi.bulletBID, hi.bt, bulletPosInArray);
 		p->hitByBullet(theBullet, hi.newHPtotal);	//Add support for hacking dart
