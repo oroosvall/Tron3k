a28088bb1b4f608fc60c1e8d4ecb33b0dd3b964d
ResQQ <resq@live.se>
2016-01-26 10:21:02 +0100

    asd

--- Tron3k/Debug/GameFiles/Shaders/portal_fs.glsl
+++ Tron3k/Debug/GameFiles/Shaders/portal_fs.glsl
@@ -23,3 +23,17 @@ void main ()
 	GlowMap = vec4(1.25 - transp , 0 , transp, transp / 4);
     GlowMap.w = 0.2;
 }
+
+//float maxlen = 20;
+//float transp = length(dist);
+//
+//if(transp > maxlen)
+//	transp = 0.0;
+//else
+//	transp =  1 - (transp / maxlen);
+   //
+//WorldPosOut	= vec4(0);
+//DiffuseOut = vec4(0);
+//NormalOut = vec4(0);
+//GlowMap = vec4(transp , 0 , transp, transp / 4);
+   //GlowMap.w = 0.2;
