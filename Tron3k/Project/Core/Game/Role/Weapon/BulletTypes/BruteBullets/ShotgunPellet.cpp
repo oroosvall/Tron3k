b7f54114c5a0b036fc064c799b47598117732018
Kraetyz <krae_tyz@hotmail.com>
2016-02-03 10:41:32 +0100

    Minor tweak
    Jag har typ inget att göra idag :cake:

--- Tron3k/Project/Core/Game/Role/Weapon/BulletTypes/BruteBullets/ShotgunPellet.cpp
+++ Tron3k/Project/Core/Game/Role/Weapon/BulletTypes/BruteBullets/ShotgunPellet.cpp
@@ -4,7 +4,7 @@
 ShotgunPellet::ShotgunPellet(glm::vec3 position, glm::vec3 direction, int pID, int bID, int tID)
 {
 	initValues(position, direction, pID, bID, tID);
-	vel = glm::vec3(65.0f);
+	vel = glm::vec3(75.0f);
 	damage = 10;
 	ttl = 1.0f;
 }
