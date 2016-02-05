beb1a7667bc8e7065faad51151e79772ae383d4f
Kraetyz <krae_tyz@hotmail.com>
2016-02-05 08:57:19 +0100

    Only 4 local

--- Tron3k/Project/Network/Topology.h
+++ Tron3k/Project/Network/Topology.h
@@ -606,7 +606,7 @@ public:
 		}
 		//Adds player to the game in chosen team. Both clients and server
 		gamePtr->addPlayerToTeam(p_conID, team);
-		if (isClient)
+		if (conID == getConId())
 		{
 			uiPtr->setFirstMenuSet(false);
 			uiPtr->setMenu(2);
