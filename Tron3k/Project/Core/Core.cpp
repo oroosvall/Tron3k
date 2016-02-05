1b14c5daba7702c0a970c4d034d3ddd6fbf229e7
ResQQ <resq@live.se>
2016-02-05 09:07:10 +0100

    playback speed for oscar

--- Tron3k/Project/Core/Core.cpp
+++ Tron3k/Project/Core/Core.cpp
@@ -164,30 +164,30 @@ void Core::update(float dt)
 				CameraInput::getCam()->setPlaybackSpeed(1.0f);
 		}
 	}
-	//if (game)
-	//{
-	//	if (console.getInChatMode() == false)
-	//	{
-	//		if (i->justPressed(GLFW_KEY_7))
-	//		{
-	//			playbackSpeed *= 2.0f;
-	//			if (playbackSpeed > 16.0f)
-	//				playbackSpeed = 16.0f;
-	//			CameraInput* cam = CameraInput::getCam();
-	//			cam->setPlaybackSpeed(playbackSpeed);
-	//		}
-	//		if (i->justPressed(GLFW_KEY_6))
-	//		{
-	//			playbackSpeed *= 0.5f;
-	//			if (playbackSpeed < 0.03125f)
-	//				playbackSpeed = 0.03125f;
-	//			CameraInput* cam = CameraInput::getCam();
-	//			cam->setPlaybackSpeed(playbackSpeed);
-	//		}
-	//	}
-	//}
+	if (game)
+	{
+		if (console.getInChatMode() == false)
+		{
+			if (i->justPressed(GLFW_KEY_7))
+			{
+				playbackSpeed *= 2.0f;
+				if (playbackSpeed > 16.0f)
+					playbackSpeed = 16.0f;
+				CameraInput* cam = CameraInput::getCam();
+				cam->setPlaybackSpeed(playbackSpeed);
+			}
+			if (i->justPressed(GLFW_KEY_6))
+			{
+				playbackSpeed *= 0.5f;
+				if (playbackSpeed < 0.03125f)
+					playbackSpeed = 0.03125f;
+				CameraInput* cam = CameraInput::getCam();
+				cam->setPlaybackSpeed(playbackSpeed);
+			}
+		}
+	}
 
-	//dt *= playbackSpeed;
+	dt *= playbackSpeed;
 
 	switch (current)
 	{
