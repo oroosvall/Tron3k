233ded30a7d56958019dafc3be7f787a396b0bb8
Kraetyz <krae_tyz@hotmail.com>
2016-02-17 12:18:10 +0100

    Increased vacuum range

--- Tron3k/Project/Core/Game/Role/Weapon/BulletTypes/ManipulatorBullets/VacuumGrenade.cpp
+++ Tron3k/Project/Core/Game/Role/Weapon/BulletTypes/ManipulatorBullets/VacuumGrenade.cpp
@@ -4,7 +4,7 @@
 VacuumGrenade::VacuumGrenade(glm::vec3 position, glm::vec3 direction, int pID, int bID, int tID)
 {
 	initValues(position, direction, pID, bID, tID);
-	vel = glm::vec3(20.0f);
+	vel = glm::vec3(35.0f);
 	damage = 10;
 }
 
