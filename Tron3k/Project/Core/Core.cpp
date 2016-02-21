8185c761644f2dffec0bf313c41daad10e6996c6
ResQQ <resq@live.se>
2016-02-21 17:57:00 +0100

    lights on bullets!

--- Tron3k/Project/Core/Core.cpp
+++ Tron3k/Project/Core/Core.cpp
@@ -1630,6 +1630,7 @@ void Core::renderWorld(float dt)
 		vec3 camPos = cam->getPos();
 		vec3 camDir = cam->getDir();
 		bool force3rd = false;
+
 		/*		if (i->getKeyInfo(GLFW_KEY_P))
 		{
 		cam->setCam(vec3(-6, 1.5f, 33), vec3(0, 0, -1));
@@ -1767,7 +1768,8 @@ void Core::renderWorld(float dt)
 					light.Direction = vec3(0.0f);//p->getDir();
 					light.Color = dgColor;
 					light.DiffuseIntensity = 0.2f;
-					light.AmbientIntensity = 0.0f;
+					light.AmbientIntensity = 0.5f;
+					light.attenuation.w = 5.0f;
 					renderPipe->addLight(&light, p->roomID);
 
 					if (p->forceDeathAnim > 0)
@@ -1828,10 +1830,20 @@ void Core::renderWorld(float dt)
 				for (unsigned int i = 0; i < bullets.size(); i++)
 				{
 					if (bullets[i]->getTeam() == 1)
+					{
 						renderPipe->renderBullet(c, bullets[i]->getWorldMat(), &TEAMONECOLOR.x, 0.0f);
+						light.Color = TEAMONECOLOR;
+						light.Position = bullets[i]->getPos();
+						renderPipe->addLight(&light, 0);
+					}
 
-					else if (bullets[i]->getTeam() == 2)
+					else //(bullets[i]->getTeam() == 2)
+					{
 						renderPipe->renderBullet(c, bullets[i]->getWorldMat(), &TEAMTWOCOLOR.x, 0.0f);
+						light.Color = TEAMTWOCOLOR;
+						light.Position = bullets[i]->getPos();
+						renderPipe->addLight(&light, 0);
+					}
 				}
 			}
 		}
@@ -1845,7 +1857,7 @@ void Core::renderWorld(float dt)
 					if (bullets[i]->getTeam() == 1)
 						renderPipe->renderBullet(c, bullets[i]->getWorldMat(), &TEAMTWOCOLOR.x, 0.0f);
 
-					else if (bullets[i]->getTeam() == 2)
+					else //if (bullets[i]->getTeam() == 2)
 						renderPipe->renderBullet(c, bullets[i]->getWorldMat(), &TEAMTWOCOLOR.x, 0.0f);
 				}
 			}
@@ -1860,7 +1872,7 @@ void Core::renderWorld(float dt)
 					if (bullets[i]->getTeam() == 1)
 						renderPipe->renderBullet(c, bullets[i]->getWorldMat(), &TEAMONECOLOR.x, 0.0f);
 
-					else if (bullets[i]->getTeam() == 2)
+					else // if (bullets[i]->getTeam() == 2)
 						renderPipe->renderBullet(c, bullets[i]->getWorldMat(), &TEAMONECOLOR.x, 0.0f);
 				}
 			}
