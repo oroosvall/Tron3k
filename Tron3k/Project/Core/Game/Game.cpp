4f249ea970643503898de4c4df5ff0b27685fd5d
Kraetyz <krae_tyz@hotmail.com>
2016-02-25 16:31:22 +0100

    pls

--- Tron3k/Project/Core/Game/Game.cpp
+++ Tron3k/Project/Core/Game/Game.cpp
@@ -1903,9 +1903,7 @@ int Game::handleBulletHitPlayerEvent(BulletHitPlayerInfo hi)
 					}
 					playerList[hi.bulletPID]->impressiveDone = true;
 				}
-				glm::vec3 hpPos = p->getPos();
-				hpPos.y += 0.2f;
-				addEffectToList(-1, p->getTeam(), hi.playerHit, EFFECT_TYPE::HEALTHPACK, hpPos, 0, 0.5f);
+				addEffectToList(-1, p->getTeam(), hi.playerHit, EFFECT_TYPE::HEALTHPACK, p->getPos(), 0, 0.5f);
 			}
 			removeBullet(hi.bt, bulletPosInArray);
 		}
@@ -2102,10 +2100,7 @@ int Game::handleEffectHitPlayerEvent(EffectHitPlayerInfo hi)
 				}
 				playerList[hi.effectPID]->impressiveDone = true;
 			}
-
-			glm::vec3 hpPos = p->getPos();
-			hpPos.y += 0.2f;
-			addEffectToList(-1, p->getTeam(), hi.playerHit, EFFECT_TYPE::HEALTHPACK, hpPos, 0, 0.5f);
+			addEffectToList(-1, p->getTeam(), hi.playerHit, EFFECT_TYPE::HEALTHPACK, p->getPos(), 0, 0.5f);
 
 			/*if (p->GetConsecutiveDeaths() > 4 && !p->roleChangeWritten)
 			{
