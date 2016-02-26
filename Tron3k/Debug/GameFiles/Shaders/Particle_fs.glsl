26b9158c3bacc7f9168263fa07bd0b93b943e643
Kraetyz <krae_tyz@hotmail.com>
2016-02-26 12:00:50 +0100

    Pow zoom kapam

--- Tron3k/Debug/GameFiles/Shaders/Particle_fs.glsl
+++ Tron3k/Debug/GameFiles/Shaders/Particle_fs.glsl
@@ -31,6 +31,6 @@ void main ()
 		NormalOut = vec4(normal, color.w * clamp(intenisty*3, 0.0f, 1.0f));
 		WorldPosOut = vec4(wPos, color.w * clamp(intenisty*3, 0.0f, 1.0f));
 		DiffuseOut = color * vec4(blendcolor, clamp(intenisty*3, 0.0f, 1.0f));
-		GlowMap = vec4(color.rgb, length(color)*intenisty * color.w);
+		GlowMap = vec4(0,0,0,0);
 	}
 }
--- Tron3k/Debug/GameFiles/Shaders/Particle_gs.glsl
+++ Tron3k/Debug/GameFiles/Shaders/Particle_gs.glsl
@@ -18,47 +18,7 @@ uniform vec3 cam;
 uniform vec2 size;
 
 void main() 
-{
-	//if(dir[0].w > 0.0f)
-	//{
-	//	vec3 pos = gl_in[0].gl_Position.xyz;
-	//	vec3 cam_normal = normalize(cam - pos);
-	//	//vec3 up = vec3(0.0f, 1.0f, 0.0f);
-	//	vec3 up = dir[0].xyz;
-	//	vec3 right = cross(cam_normal, up);
-    //
-	//	up = up * size.y*10;
-	//	right = right * size.x*10;
-    //
-	//	vec3 cPos = pos;
-    //
-	//	//VTX 1
-	//	cPos = pos - right - up;
-	//	gl_Position = VP * vec4(cPos, 1.0f);
-	//	uv_frag = vec2(0.0f, 0.0f);
-	//	EmitVertex();
-    //
-	//	//VTX 2
-	//	cPos = pos + right - up;
-	//	gl_Position = VP * vec4(cPos, 1.0f);
-	//	uv_frag = vec2(1.0f, 0.0f);
-	//	EmitVertex();
-    //
-	//	//VTX 3
-	//	cPos = pos + up - right;
-	//	gl_Position = VP * vec4(cPos, 1.0f);
-	//	uv_frag = vec2(0.0f, 1.0f);
-	//	EmitVertex();
-    //
-	//	//VTX 4
-	//	cPos = pos + up + right;
-	//	gl_Position = VP * vec4(cPos, 1.0f);
-	//	uv_frag = vec2(1.0f, 1.0f);
-	//	EmitVertex();
-	//	
-	//	EndPrimitive();
-	//}
-	
+{	
 	if(dir[0].w > 0.0f)
 	{
 		vec3 pos = gl_in[0].gl_Position.xyz;
@@ -73,7 +33,12 @@ void main()
 		vec3 v = pos;
 		
 		up *= (size.y / 2);
+
+
 		right *= (size.x / 2);
+
+		up *= iDir[0].w;
+		right *= iDir[0].w;
 		
 		normal = cam_normal;
 		
