51ed53007bfb6577a801170ed81d8fa5bc949905
ResQQ <resq@live.se>
2016-02-28 11:52:45 +0100

    Hitmarkers when spectated player deals damage

--- Tron3k/Project/Core/Core.cpp
+++ Tron3k/Project/Core/Core.cpp
@@ -2631,10 +2631,10 @@ void Core::renderWorld(float dt)
 
 		renderPipe->renderCrosshair(CROSSHAIR_TRAPPER_P);
 
-		if (game->getPlayer(game->GetLocalPlayerId())->hitMarker > 0.0f)
+		if (tmp_player->hitMarker > 0.0f)
 		{
 			renderPipe->renderCrosshair(CROSSHAIR_SHANKER_P);
-			game->getPlayer(game->GetLocalPlayerId())->hitMarker -= dt;
+			tmp_player->hitMarker -= dt;
 		}
 		
 
