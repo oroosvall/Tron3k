4ccd0c0f6a051687dcd547070a6d9e4672413da9
joeljoeljoeljoeljoel <munken_92@yahoo.se>
2016-02-29 16:34:33 +0100

    sound fix

--- Tron3k/Project/Core/Game/Game.cpp
+++ Tron3k/Project/Core/Game/Game.cpp
@@ -1520,10 +1520,10 @@ void Game::handleWeaponFire(int conID, int teamId, int bulletId, WEAPON_TYPE wea
 			if (GetSound())
 				if (conID == localPlayerId || conID == spectateID)
 				{
-					GetSound()->playFields(pos.x, pos.y, pos.z);
+					GetSound()->playFieldsStereo();
 				}
 				else
-					GetSound()->playFieldsStereo();
+					GetSound()->playFields(pos.x, pos.y, pos.z);
 
 		addBulletToList(conID, teamId, bulletId, BULLET_TYPE::BATTERY_SPEED_SHOT, pos, dir);
 		break;
